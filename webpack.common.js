9109ce6f7021f33e0c033a727d87c453d7bb6840
jeseias <domingosjeseias@gmail.com>
2021-12-26 10:23:35 +0100

    chore: config eslint

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/main/index.tsx',
@@ -9,7 +9,7 @@ module.exports = {
     publicPath: '/'
   },
   resolve: {
-    extensions: ['.ts', '.tsx','.js'],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
