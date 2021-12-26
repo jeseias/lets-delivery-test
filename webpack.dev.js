cbcbc28f8a3eaf546b840168fadf310df8a06b7c
jeseias <domingosjeseias@gmail.com>
2021-12-26 09:40:22 +0100

    chore: webpack configs

---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const { DefinePlugin } = require("webpack");
+const { default: merge } = require("webpack-merge");
+const common = require('./webpack.common')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'development',
+  modules: {
+    rules: [
+      {
+        test: /.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: '/public'
+    },
+    historyApiFallback: true,
+    port: 8080
+  },
+  plugins: [
+     new DefinePlugin({
+      'process.env.API_URL': JSON.stringify('https://psychonauts-api.netlify.app/')
+     }),
+     new HtmlWebpackPlugin({
+      template: './template.dev.html'
+     })
+  ]
+})
