3e1e5bd1c5416cc28117efba67f5d169416cea41
jeseias <domingosjeseias@gmail.com>
2021-12-27 09:27:54 +0100

    test: mockCharacterModel

--- tests/domain/mocks/mock-character.ts
+++ tests/domain/mocks/mock-character.ts
@@ -1,5 +1,16 @@
-import {} from '@/d'
+import { Character } from '@/domain/models/character'
+
+import * as faker from 'faker'
 
 export const mockCharacterModel = (): Character => ({
-
+  name: faker.name.gender(),
+  gender: faker.name.gender() as any,
+  img: faker.image.image(),
+  psiPowers: [
+    {
+      name: faker.name.title(),
+      description: faker.name.jobDescriptor(),
+      img: faker.image.abstract()
+    }
+  ]
 })
