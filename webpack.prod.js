e183b53718159bd8a1ba028c5b52c16d0b79c923
jeseias <domingosjeseias@gmail.com>
2021-12-27 22:40:09 +0100

    update api url

--- webpack.prod.js
+++ webpack.prod.js
@@ -22,7 +22,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new DefinePlugin({
-      'process.env.API_URL': JSON.stringify('https://fordevs.herokuapp.com/api')
+      'process.env.API_URL': JSON.stringify('https://psychonauts-api.herokuapp.com/api/')
     }),
     new HtmlWebpackPlugin({
       template: './template.prod.html'
